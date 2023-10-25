9723a41a4f7e0baea836607649d263a7e3b061f3
Nouman Ashraf <muhammad-nouman@hotmail.com>
2023-10-25 20:10:32 +0500

    build issue resolve

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -43,7 +43,6 @@ export const createTRPCContext = (opts: CreateNextContextOptions) => {
   // console.log("session", session);
   const userId = session.userId;
   return {
-    session,
     db,
     userId,
   };
--- src/server/helper/ssrHelper.ts
+++ src/server/helper/ssrHelper.ts
@@ -2,6 +2,7 @@ import { createServerSideHelpers } from "@trpc/react-query/server";
 import { appRouter } from "~/server/api/root";
 import superjson from "superjson";
 import { db } from "../db";
+import { Session } from "@clerk/nextjs/dist/types/server";
 
 export const generateSSRHelper = () =>
   createServerSideHelpers({
